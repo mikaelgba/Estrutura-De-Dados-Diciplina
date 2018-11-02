3e932618f356a6e5a8038d7464713ca1ca36d92f
Michael_Oliveira <33075036+mikaelgba@users.noreply.github.com>
2018-11-02 10:35:55 -0300

    Update CoutingSort.java

--- Ordenacao/CoutingSort.java
+++ Ordenacao/CoutingSort.java
@@ -1,5 +1,4 @@
 package Ordenacao;
-
 import java.util.Arrays;
 
 public class CoutingSort {
@@ -9,27 +8,22 @@ public class CoutingSort {
 	    System.out.println("Counting sort in Java");
 	    int[] input = { 60, 40, 30, 20, 10, 40, 30, 60, 60, 20, 40, 30, 40 };
 	    int k = 60;
-
 	    System.out.println(Arrays.toString(input));
-
-	    countingSort(input, k);
-
-	   
+	    countingSort(input, k);	
 	    System.out.println(Arrays.toString(input));
-
 	  }
 	public static void countingSort( int [] vetor, int k) {
 		
-		int [] aux = new int[k+1];
-		for( int i: vetor) {
-			aux[i]++;
+	    int [] aux = new int[k+1];
+	    for( int i: vetor) {
+		aux[i]++;
+	    }
+	    int aux2 = 0;
+	    for( int j = 0; j < vetor.length; j++) {
+		while(aux[j] > 0) {
+		    vetor[aux2] = j;
+		    aux[j]--;	
 		}
-		int aux2 = 0;
-		for( int j = 0; j < vetor.length; j++) {
-			while(aux[j] > 0) {
-				vetor[aux2] = j;
-				aux[j]--;	
-			}
-		}
+	    }
 	}
 }
