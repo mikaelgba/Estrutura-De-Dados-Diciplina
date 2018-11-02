fe8143fbf5fb565651579f05bbce54312c02ff90
Michael_Oliveira <33075036+mikaelgba@users.noreply.github.com>
2018-11-02 10:30:14 -0300

    Update SelectionSort.java

--- Ordenacao/SelectionSort.java
+++ Ordenacao/SelectionSort.java
@@ -1,5 +1,4 @@
 package Ordenacao;
-
 import java.io.IOException;
 import java.util.Arrays;
 
@@ -7,19 +6,13 @@ public class SelectionSort {
 	
 	public static void main(String[] args) throws IOException {
 
-		System.out.println("Selection Sort in Java");
+	    System.out.println("Selection Sort in Java");
 	    int[] input = { 60, 40, 30, 20, 10, 40, 30, 60, 60, 20, 40, 30, 40 };
-
 	    System.out.println(Arrays.toString(input));
-
 	    selectionSort(input);
-
 	    System.out.println(Arrays.toString(input));
-
-	}
-	
-	public static void selectionSort( int [] vetor) {
-		
+	}	
+	public static void selectionSort( int [] vetor) {		
 		for( int i = 0; i < vetor.length - 1; i++) {
 			int menor = i;
 			for( int j = i + 1; j < vetor.length; j++) {
