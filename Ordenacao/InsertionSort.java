c82cc35035ac381aff75f220d2904822e4a0085f
Michael_Oliveira <33075036+mikaelgba@users.noreply.github.com>
2018-11-02 10:34:34 -0300

    Update InsertionSort.java

--- Ordenacao/InsertionSort.java
+++ Ordenacao/InsertionSort.java
@@ -1,5 +1,4 @@
 package Ordenacao;
-
 import java.io.IOException;
 import java.util.Arrays;
 
@@ -7,27 +6,23 @@ public class InsertionSort {
 	
 	public static void main(String[] args) throws IOException {
 
-		System.out.println("Insertion Sort in Java");
+	    System.out.println("Insertion Sort in Java");
 	    int[] input = { 60, 40, 30, 20, 10, 40, 30, 60, 60, 20, 40, 30, 40 };
-
 	    System.out.println(Arrays.toString(input));
-
 	    insertionSort(input);
-
 	    System.out.println(Arrays.toString(input));
-
 	}
 	public static void insertionSort( int [] vetor) {
 		
-		int troca = 0;
-		for( int i = 0; i < vetor.length; i++) {
-			for( int j = i; j > 0; j--) {
-				if( vetor[j] < vetor[j-1]) {
-					troca = vetor[j];
-					vetor[j] = vetor[j-1];
-					vetor[j-1] = troca;
-				}
-			}
+	    int troca = 0;
+	    for( int i = 0; i < vetor.length; i++) {
+		for( int j = i; j > 0; j--) {
+		    if( vetor[j] < vetor[j-1]) {
+			troca = vetor[j];
+			vetor[j] = vetor[j-1];
+			vetor[j-1] = troca;
+		    }
 		}
+	    }
 	}
 }
