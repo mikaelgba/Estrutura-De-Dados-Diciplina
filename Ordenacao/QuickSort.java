f9842f253752d9ce0a992cdfd16344a1af943d22
Michael_Oliveira <33075036+mikaelgba@users.noreply.github.com>
2018-11-02 10:32:44 -0300

    Update QuickSort.java

--- Ordenacao/QuickSort.java
+++ Ordenacao/QuickSort.java
@@ -1,5 +1,4 @@
 package Ordenacao;
-
 import java.io.IOException;
 import java.util.Arrays;
 
@@ -9,14 +8,11 @@ public class QuickSort {
 
 		System.out.println("Quick Sort in Java");
 		int[] input = { 60, 40, 30, 20, 10, 40, 30, 60, 60, 20, 40, 30, 40 };
-
 		System.out.println(Arrays.toString(input));
 		quickSort(input, 0, (input.length)-1);
 		System.out.println(Arrays.toString(input));
-
 	}
 	private static void quickSort(int[] vetor, int inicio, int fim) {
-
 		if (inicio < fim) {
 		   int posicaoPivo = separar(vetor, inicio, fim);
 		   quickSort(vetor, inicio, posicaoPivo - 1);
