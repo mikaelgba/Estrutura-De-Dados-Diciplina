7e1354caf5b20fc89481106fcf0663954f40b3c5
Michael_Oliveira <33075036+mikaelgba@users.noreply.github.com>
2018-11-02 10:32:09 -0300

    Update BuscaBinaria.java

--- Ordenacao/BuscaBinaria.java
+++ Ordenacao/BuscaBinaria.java
@@ -1,5 +1,4 @@
 package Ordenacao;
-
 import java.io.IOException;
 import java.util.Arrays;
 
@@ -9,20 +8,16 @@ public class BuscaBinaria {
 
 		System.out.println("Busca Binaria in Java");
 		int[] input = { 60, 40, 30, 20, 10, 40, 30, 60, 60, 20, 40, 30, 40 };
-
 		System.out.println(Arrays.toString(input));
 		SelectionSort.selectionSort(input);
-
 		System.out.println(Arrays.toString(input));
 		System.out.println(buscaBinaria(input, 20));
-
 	}
 	public static boolean buscaBinaria(int[] input, int k) {
 
 		int meio;
 		int inicio = 0;
-		int fim = (input.length) - 1;
-		
+		int fim = (input.length) - 1;	
 		while (inicio <= fim) {
 			meio = (inicio + fim) / 2;
 			if (k < input[meio]) {
