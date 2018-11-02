2879c6b9dff1a9135a5e82cf3f8c58b2ef2b1233
Michael_Oliveira <33075036+mikaelgba@users.noreply.github.com>
2018-11-02 10:29:22 -0300

    Update QuickSort.java

--- Ordenacao/QuickSort.java
+++ Ordenacao/QuickSort.java
@@ -11,30 +11,28 @@ public class QuickSort {
 		int[] input = { 60, 40, 30, 20, 10, 40, 30, 60, 60, 20, 40, 30, 40 };
 
 		System.out.println(Arrays.toString(input));
-
 		quickSort(input, 0, (input.length)-1);
-
 		System.out.println(Arrays.toString(input));
 
 	}
 	private static void quickSort(int[] vetor, int inicio, int fim) {
 
 		if (inicio < fim) {
-			int posicaoPivo = separar(vetor, inicio, fim);
-			quickSort(vetor, inicio, posicaoPivo - 1);
-			quickSort(vetor, posicaoPivo + 1, fim);
+		   int posicaoPivo = separar(vetor, inicio, fim);
+		   quickSort(vetor, inicio, posicaoPivo - 1);
+		   quickSort(vetor, posicaoPivo + 1, fim);
 		}
 	}
 	private static int separar(int vetor[], int inicio, int fim){
 		
-		int pivo = vetor[fim];
-		int aux = inicio - 1; 
+	    int pivo = vetor[fim];
+	    int aux = inicio - 1; 
 	    for( int j = inicio; j < fim; j++) {
 	    	if( pivo >= vetor[j]) {
-	    		aux++;
-	    		int troca = vetor[aux];
-	    		vetor[aux] = vetor[j];
-	    		vetor[j] = troca;
+	    	   aux++;
+	    	   int troca = vetor[aux];
+	    	   vetor[aux] = vetor[j];
+	    	   vetor[j] = troca;
 	    	}
 	    }
 	    int troca = vetor[aux+1];
